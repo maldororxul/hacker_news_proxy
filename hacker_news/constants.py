2d36b1e17c0efecec91f2c8e23072bca9f4118af
maldororxul <studia-kirill@yandex.ru>
2022-05-12 15:53:32 +0400

    fixes 2

--- hacker_news/constants.py
+++ hacker_news/constants.py
@@ -3,6 +3,6 @@ import re
 __author__ = 'ke.mizonov'
 
 BASE_URL = 'https://news.ycombinator.com'
-HOME_PAGE = ''
-REPLACEMENT_PATTERN = re.compile(r'\b[a-zA-Z]{6}\b')
+HOME_PAGE = '/'
+REPLACEMENT_PATTERN = re.compile(r'\s?[а-яёА-ЯЁa-zA-Z]{6}\s')
 TRADEMARK = u'\u2122'
