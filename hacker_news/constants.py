32f7e3407525f79bf48a3eb5f93d5f0bb816eed4
maldororxul <studia-kirill@yandex.ru>
2022-05-12 21:59:41 +0400

    fixes 3

--- hacker_news/constants.py
+++ hacker_news/constants.py
@@ -3,6 +3,9 @@ import re
 __author__ = 'ke.mizonov'
 
 BASE_URL = 'https://news.ycombinator.com'
+CSS_EXT = '.css'
+CSS_RESOURCES_PATTERN = re.compile(r"""url\(\"[a-z0-9.]+\"\)""")
+CSS_URL_PATTERN = re.compile(r"""\"[a-z0-9.]+\"""")
 HOME_PAGE = '/'
 REPLACEMENT_PATTERN = re.compile(r'\s?[а-яёА-ЯЁa-zA-Z]{6}\s')
 TRADEMARK = u'\u2122'
--- hacker_news/views.py
+++ hacker_news/views.py
@@ -1,5 +1,6 @@
 """Views handlers for Hacker News proxy"""
 __author__ = 'ke.mizonov'
+import os
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -7,7 +8,17 @@ from bs4.element import Tag
 from dataclasses import dataclass, field
 from django.http import Http404, HttpRequest, HttpResponse
 from django.http.response import HttpResponseNotFound
-from .constants import BASE_URL, HOME_PAGE, REPLACEMENT_PATTERN, TRADEMARK
+from urllib.request import urlretrieve
+from project.settings import BASE_DIR, STATIC_URL
+from .constants import (
+    BASE_URL,
+    CSS_EXT,
+    CSS_RESOURCES_PATTERN,
+    CSS_URL_PATTERN,
+    HOME_PAGE,
+    REPLACEMENT_PATTERN,
+    TRADEMARK
+)
 
 
 @dataclass(frozen=True)
@@ -50,6 +61,30 @@ class HackerNews:
         elem.string.replace_with(modified)
 
     @classmethod
+    def __download_static_file(cls, file: str):
+        """Downloads resources
+
+        Args:
+            file: file name with extension
+        """
+        # make dirs and download file
+        os.makedirs(os.path.join(BASE_DIR, STATIC_URL), exist_ok=True)
+        file = file.split("?")[0]
+        file_path = f'{STATIC_URL}{file}'
+        # skip files that had already been downloaded (fixme: file updated on source)
+        if os.path.isfile(file_path):
+            return
+        urlretrieve(f'{cls.base_url}/{file}', file_path)
+        # css files may contain more resources
+        _, ext = os.path.splitext(file_path)
+        if ext != CSS_EXT:
+            return
+        with open(file_path, 'r') as css_file:
+            for css_resource in CSS_RESOURCES_PATTERN.findall(css_file.read()):
+                css_url = CSS_URL_PATTERN.findall(css_resource)[0].strip('"')
+                cls.__download_static_file(file=css_url)
+
+    @classmethod
     def __fetch(cls, request: HttpRequest) -> requests.Response:
         """Makes request to the source with params
 
@@ -69,6 +104,21 @@ class HackerNews:
         return response
 
     @classmethod
+    def __handle_resources(cls, soup: BeautifulSoup.text, tag: str, attr: str):
+        """Modifies resources by adding base url as attr value (href or src) for specified tag elements in soup
+
+        Args:
+            soup: bs4 soup
+            tag: tag to seek for in soup
+            attr: attr to seek for in soup
+        """
+        for elem in soup.find_all(tag) or []:
+            # download static files (with dirty favicon hack)
+            file = elem[attr]
+            cls.__download_static_file(file=file)
+            elem[attr] = f"{STATIC_URL}{file}"
+
+    @classmethod
     def __modify_html_data(cls, response: requests.Response) -> bytes:
         """Modifies HTML-content
 
@@ -84,21 +134,9 @@ class HackerNews:
             cls.__append_suffix_by_pattern(elem=elem, pattern=REPLACEMENT_PATTERN, suffix=TRADEMARK)
         # change imgs, scripts and links to "look at" original site recources
         for tag, attr in (('img', 'src'), ('script', 'src'), ('link', 'href')):
-            cls.__modify_resources(soup=soup, tag=tag, attr=attr)
+            cls.__handle_resources(soup=soup, tag=tag, attr=attr)
         # change home-page links
         for elem in soup.find_all(href=True) or []:
             if elem['href'] == cls.base_url:
                 elem['href'] = HOME_PAGE
         return soup.prettify("utf-8")
-
-    @classmethod
-    def __modify_resources(cls, soup: BeautifulSoup.text, tag: str, attr: str):
-        """Modifies resources by adding base url as attr value (href or src) for specified tag elements in soup
-
-        Args:
-            soup: bs4 soup
-            tag: tag to seek for in soup
-            attr: attr to seek for in soup
-        """
-        for elem in soup.find_all(tag) or []:
-            elem[attr] = f"{cls.base_url}/{elem[attr]}"
